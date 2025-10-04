a8c40fcbbbf48da8c38049c21b937e995522159e
Khamidulla <khamidulla_orifov@epam.com>
2025-10-04 19:57:36 +0500

    Implemented adding new morty from outside

--- Morties/MortyFactory.cs
+++ Morties/MortyFactory.cs
@@ -1,14 +1,21 @@
-﻿namespace MortyGame.Morties;
+﻿using System.Reflection;
+
+namespace MortyGame.Morties;
 
 public class MortyFactory
 {
     public static IMorty CreateMorty(string className, int boxCount)
     {
-        return className switch
-        {
-            "ClassicMorty" => new ClassicMorty(boxCount),
-            "LazyMorty" => new LazyMorty(boxCount),
-            _ => throw new ArgumentException($"Unknown Morty class name: {className}")
-        };
+        var mortyType = Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .FirstOrDefault(t => typeof(IMorty).IsAssignableFrom(t) &&
+                                     !t.IsInterface &&
+                                     !t.IsAbstract &&
+                                     t.Name.Equals(className, StringComparison.OrdinalIgnoreCase));
+
+        if (mortyType == null)
+            throw new ArgumentException($"Unknown Morty class name: {className}");
+
+        return (IMorty)Activator.CreateInstance(mortyType, boxCount)!;
     }
 }
